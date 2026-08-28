c524115b0baf3ee736446ec7aece054529767ea8
sahilsingh-svg <sahil.singh@flodataanalytics.com>
2026-08-28 19:24:28 +0530

    Update CORS to allow Vercel domains

--- web.py
+++ web.py
@@ -108,6 +108,7 @@ app = FastAPI(title="B2B Prospecting Agent", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001", "http://127.0.0.1:3001"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
